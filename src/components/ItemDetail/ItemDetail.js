20953dcc5c9a9086a3067cf6c69f7b50a8d38470
Flor Sabatino <florencia.sabatino@gmail.com>
2022-08-06 19:11:58 -0300

    actualizacion

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,9 @@
 import './ItemDetail.scss'
+import ItemCount from '../ItemCount/ItemCount'
+
 
 const ItemDetail = ({data}) => {
+    const {id,title,imagen,price,stock} =data
     return (
         <>
         <div className="item-detail-image">
@@ -8,7 +11,6 @@ const ItemDetail = ({data}) => {
         </div>
         <div className="item-detail-info">
             <span className="categoria">Mujer - Sweater</span>
-            <h3>{data.title}</h3>
             <p className="detail-info-precio"> $ {data.price}</p>
             <div className="detail-info-color">
                 <button>Verde Militar</button> / <button>Manteca</button>
@@ -18,10 +20,25 @@ const ItemDetail = ({data}) => {
                 <button>S</button>
                 <button>M</button>
             </div>
+          <ItemCount cantidad={stock}/> 
             <button>Añadir al Carrito</button>
         </div>
+        <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{data.title}</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Cantidad disponible: {stock}</small></p>
+      </div>
+    </div>
+  </div>
+</div>
         </>
-    )
+    );
 }
 
 export default ItemDetail
--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,12 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './ItemProduct.scss';
-import ItemCount from '../ItemCount/ItemCount';
 import {Link} from 'react-router-dom'
 
 
 const ItemProduct = ({data, action}) => {
-  const {id,title,imagen,price,stock} =data
+  const {id,title,imagen,price} =data
   return (
     <div className='item-product'>
     
@@ -19,11 +18,7 @@ const ItemProduct = ({data, action}) => {
           {title}
         </Card.Text>
         </Link>
-        <ItemCount cantidad={stock}/> 
-        <Card.Text>
-          Cantidad disponible: {stock}
-        </Card.Text>
-                <Button onClick={action} className="BotonAgregar"variant="primary">Agregar al carrito</Button>
+        <Button onClick={action} className="BotonAgregar"variant="primary">Agregar al carrito</Button>
       </Card.Body>  
     </Card>
     
