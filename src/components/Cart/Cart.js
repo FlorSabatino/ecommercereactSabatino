0fa437ce7191b75957e8fb6b212c6c7ba0f99d20
Flor Sabatino <florencia.sabatino@gmail.com>
2022-08-16 20:26:01 -0300

    actualizacion cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,35 @@
-import {useContext} from 'react'
-import {CartContext} from '../../context/CartContext'
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const Cart = () => {
 
-    const {cartProducts, restarProducto, addNumber, removeItem} = useContext(CartContext)
-        {cartProducts.map((product)=>{
-            return (
-    <div className='item-cart-product' key={product.id} >
-        <img style={{height: 100}} src={`${product.imagen}`} alt="" />
-        <div className='cart-product-details'>
-        <span>{product.title}</span>         
-        </div>
-    <div className='cart-product-details'>
-        <span>$ {product.price}</span>
-        <button className='botonesDesplegable'onClick={() => restarProducto ()} >-</button>
-        <span>{product.sumarYrestar}</span>
-        <button className='botonesDesplegable'onClick={() => addNumber ()} >+</button>
-        <button className='botonesDesplegable'onClick={() => removeItem (product.id)}> <DeleteIcon/></button>
-    </div>
-    </div>
+    const { cartProducts, restarProducto, addNumber, removeItem } = useContext(CartContext)
+
+    return (
+        <>
+
+        {
+            cartProducts.map((product) => {
+                return (
+                    <div className='item-cart-product' key={product.id} >
+                        <img style={{ height: 100 }} src={`${product.imagen}`} alt="" />
+                        <div className='cart-product-details'>
+                            <span>{product.title}</span>
+                        </div>
+                        <div className='cart-product-details'>
+                            <span>$ {product.price}</span>
+                            <button onClick={() => restarProducto()} >-</button>
+                            <span>{product.sumarYrestar}</span>
+                            <button onClick={() => addNumber()} >+</button>
+                            <button onClick={() => removeItem(product.id)}> <DeleteIcon /></button>
+                        </div>
+                    </div>
+                )
+    
+            })
+        }
+        </>
     )
-        
-})}
 }
 export default Cart;
